6183c234e0382354e9ac450139557bc200cd8f12
Joe Mbome <joembome@yahoo.com>
2021-03-19 17:29:13 -0400

    update

--- app.js
+++ app.js
@@ -104,6 +104,21 @@ alertBanner.addEventListener('click', e => {
 });
 
 
+const send = document.querySelector('#send');
+const user = document.querySelector('#userField');
+const message = document.querySelector('#messageField');
 
+send.addEventListener('click', e => {
+  e.preventDefault();
+  if (user.value === "" && message.value === "") {
+    alert("Please fill out user and message fields before sending");
+  } else if (user.value === "") {
+    alert("Please fill out user field before sending");
+  } else if (message.value === "") {
+    alert("Please fill out message field before sending");
+  } else {
+    alert(`Message successfully sent to: ${user.value}`);
+  }
+});
   
 
